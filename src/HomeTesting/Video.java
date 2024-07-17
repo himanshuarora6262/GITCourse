3248b05fa385513168e2acf160f2f2527f4e2d3e
Himanshu <Himanshu.arora6262@gmail.com>
2024-07-17 13:41:55 +0530

    Feature Branch

---
+++ src/HomeTesting/Video.java
@@ -0,0 +1,18 @@
+package HomeTesting;
+
+public class Video {
+
+	public static void main(String[] args) {
+
+		// Integer Array in Reverse Order
+		
+		int[] myArray = {23,34,2,10};
+		
+		for (int i=myArray.length-1;i>=0;i--)
+		{
+			System.out.println(myArray[i]);
+		}
+
+	}
+
+}
